69c42ac22c95ab81721b5e1bf65fcc807417ffaa
jokerase <47047265+jokerase@users.noreply.github.com>
2020-06-24 22:11:50 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -100,7 +100,7 @@ function Install_ct()
     check_sys
     check_new_ver
     `rm -rf gost-linux-"$bit"-"$ct_new_ver".gz`
-    `wget --no-check-certificate https://yunpan.sk222.cf/Cloud/gost.gz`
+    `wget --no-check-certificate https://yunpan.kyxz.ml/Cloud/gost.gz`
     `gunzip gost.gz`
     `mv gost-linux-"$bit"-"$ct_new_ver" gost`
     `mv gost /usr/bin/gost`
